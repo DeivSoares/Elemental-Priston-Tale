478cbf225b38accd56abcb62e8777308a4cd82f9
Deivison Soares <deivisonsoares1610@outlook.com>
2022-03-26 19:25:11 -0300

    Update

---
+++ php/index.php
@@ -0,0 +1,283 @@
+﻿<!DOCTYPE html>
+<html>
+
+
+<head>
+	<meta charset="utf-8" />
+	<title>Elemental Priston Tale - 2022</title>
+	<link rel="shortcut icon" href="icon.png" />
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="dreamer.css" rel="stylesheet" media="screen">
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+	<link href="css/profiles.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+	<div id="navbar">
+		<ul class="menu flex-c-c" "><li><a href=" index.php">Home</a></li>
+			<li><a target="_blank" href="painel/index.php">Painel do Jogador</a></li>
+			<li><a href="download.php">Downloads</a></li>
+			<li><a href="https://discord.gg/b6PGaSSgdd" target="_blank">Discord</a></li>
+			<li><a href="donate.php">Donate</a></li>
+			<li><a href="rankings.php">Rankings</a></li>
+			<li><a href="guia.php">Guia do Jogo</a></li>
+		</ul>
+	</div>
+	<li><a href="MIX.php"></a></li>
+	</ul>
+	</div>
+	<div class="wrapper_web">
+		<header class="header_web">
+
+			<div class="logo">
+				<a href="index.php"><img src="images/banner.png" alt=""></a><br>
+			</div>
+		</header>
+		<div class="container_web">
+			<aside class="left-sidebar sidebar">
+				<div class="download-block"> <a href="download.php" class="flex-c-c"><span>Download<b>Jogue Gratuitamente!</b></span></a> </div>
+				<div class="widget red-dark">
+					<div class="widget-title"> Top 10 Level <span>Os Melhores Jogadores!</span> </div>
+					<ul class="top-block">
+						<iframe name="1" border="0" frameborder="0" height="350" title="-" scrolling="no" align="top" src="http://elementalpt.ddns.net/include/index.php" width="240">
+						</iframe>
+				</div>
+
+				<div class="widget red-dark">
+					<div class="widget-title"> Boss List <span>Horário dos Boss's</span></div>
+					<ul class="eventBody top-block">
+						<li class="top-list event-schedule">
+							<div style="width:50%;">
+								<span id="bloodcastle_name"></span>
+								<a href="bosstime.php"><span class="smalltext"><b>Valento</b> - Confira </span></a>
+							</div>
+							<div style="width:50%;" class="text-right">
+								<span id="bloodcastle_next"></span>
+								<span class="smalltext" id="bloodcastle"></span>
+							</div>
+						</li>
+						<li class="top-list event-schedule">
+							<div style="width:50%;">
+								<span id="devilsquare_name"></span>
+								<a href="bosstime.php"><span class="smalltext"><b>Kelvezu</b> - Confira </span></a>
+							</div>
+							<div style="width:50%;" class="text-right">
+								<span id="devilsquare_next"></span>
+								<span class="smalltext" id="devilsquare"></span>
+							</div>
+						</li>
+						<li class="top-list event-schedule">
+							<div style="width:50%;">
+								<span id="chaoscastle_name"></span>
+								<a href="bosstime.php"><span class="smalltext"><b>Valento</b> - Confira </span></a>
+							</div>
+							<div style="width:50%;" class="text-right">
+								<span id="chaoscastle_next"></span>
+								<span class="smalltext" id="chaoscastle"></span>
+							</div>
+						</li>
+						<li class="top-list event-schedule">
+							<div style="width:50%;">
+								<span id="dragoninvasion_name"></span>
+								<a href="bosstime.php"><span class="smalltext"><b>Tulla</b> - Confira </span></a>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="widget red">
+				</div>
+			</aside>
+			<main class="content">
+				<div class="slider">
+					<div class="next arrows"></div>
+					<div class="prev arrows"></div>
+					<div class="slides">
+						<div class="slide active" style="background-image: url(images/01.jpg);">
+						</div>
+					</div>
+					<div class="navigation">
+					</div>
+				</div>
+				<section class="content-wrapper">
+					<div class="panel panel-news">
+
+						<div class="panel-heading">
+
+							<h3 class="panel-title"><img border="0" src="images/promocao.png"> Confira nossa tabela de Aging!</h3>
+
+						</div>
+
+						<div class="panel-body">
+
+							<p><strong>Olá Jogadores,</strong></p>
+
+							<p>Quer conferir nossa tabela de aging?<br>
+								<strong>Clique <a href="mix.php">aqui</a>!</strong> <br>
+								Não se esqueça! Convide os seus amigos, curtam nossa página do <a href="https://www.facebook.com/ElementalPT" target="_blank">facebook</a> e seja bem vindo ao ElementalPT!
+							</p>
+
+						</div>
+
+						<div class="panel-footer">
+
+							<div class="col-xs-6 nopadding"></div>
+
+							<div class="col-xs-6 nopadding text-right">
+
+								Postado por Administrator, Março - 2022
+
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-news">
+						<div class="panel-heading">
+							<h3 class="panel-title"><img border="0" src="images/promocao.png"> Site Liberado!</h3>
+						</div>
+						<div class="panel-body">
+							<p><strong>Olá Jogadores,</strong></p>
+							<p>Como prometemos a todos vocês, nosso siste está pronto! convide os seus amigos, curtam nossa página do facebok e seja bem vindo ao ElementalPT!</p>
+						</div>
+						<div class="panel-footer">
+							<div class="col-xs-6 nopadding"></div>
+							<div class="col-xs-6 nopadding text-right">
+								Postado por Administrator, Fevereiro - 2022</div>
+						</div>
+					</div>
+					<div class="panel panel-news">
+						<div class="panel-heading">
+							<h3 class="panel-title"><a href="#"><img border="0" src="images/promocao.png"></a>
+								Inauguração do ElementalPT!</h3>
+						</div>
+						<div class="panel-body">
+							<p><strong>Olá Jogadores,</strong></p>
+							<p>Estamos trazendo para vocês o nosso novo projeto de Priston Tale, o Elemental
+								Priston Tale. Um novo servidor voltado ao Up, Com muitas novidades que
+								vão fazer você se apaixonar<br>
+								pelo nosso servidor!</p>
+							<p>Para mais Informações, pedimos que vocês curtam nossa página e nos ajudem na
+								divulgação!</p>
+						</div>
+						<div class="panel-footer">
+							<div class="col-xs-6 nopadding"></div>
+							<div class="col-xs-6 nopadding text-right">Postado por Administrator, Fevereiro - 2022</div>
+						</div>
+					</div>
+				</section>
+			</main>
+			<aside class="right-sidebar sidebar">
+				<div class="status-block flex-c-c">
+					<div class="server-x">
+
+						<head>
+							<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+						</head>
+						<!--
+var mensagem="";
+function clickIE() {if (document.all) {(mensagem);return false;}}
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(mensagem);return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+document.oncontextmenu=new Function("return false")
+// -->
+						</script>
+						<font face='Arial' style=font-size:10pt; color=#FFFFFF>
+							<b>
+					</div>
+					<div class="server">
+						<div class="flex-s-c">
+							<span class="server-name">ONLINE<iframe border="0" frameborder="0" title="-" scrolling="no">
+								</iframe></span><span class="status-online"><b>
+								</b></span>
+						</div>
+					</div>
+				</div>
+				<div class="widget red-dark">
+					<div class="widget-title">Rates do Servidor<span></span> </div>
+					<table class="table" style="color:#face8f; font-size:12px;">
+						<tbody>
+
+							<tr>
+								<td>Level Inicial:</td>
+								<td>01 kit (12h)</td>
+							</tr>
+
+							<tr>
+								<td>Bônus New Player</td>
+								<td>Até Level 60</td>
+							</tr>
+
+							<tr>
+								<td>EXP Rate</td>
+								<td>20x</td>
+							</tr>
+
+							<tr>
+								<td>Level Máximo</td>
+								<td>102</td>
+							</tr>
+
+							<tr>
+								<td>Aging Máx:</td>
+								<td>+16</td>
+							</tr>
+
+							<tr>
+								<td>Bless Castle</td>
+								<td>Domingo,16 horas!</td>
+							</tr>
+
+						</tbody>
+					</table>
+				</div>
+				<div class="widget red-dark">
+					<div class="widget-title">Bless Castle<span>Clan vencedor do Castelo!</span></div>
+					<div id="top_guilds" style="text-align: center;">
+						<iframe name="I1" border="0" frameborder="0" height="143" title="-" scrolling="no" align="top" src="http://elementalpt.ddns.net/include/liderbc.php" width="219"></iframe>
+					</div>
+				</div>
+				<div class="widget red-dark">
+					<div class="widget-title">
+						Belatra<span>Clan vencedor de SOD!</span></div>
+					<div id="top_guilds0" style="text-align: center;">
+						<iframe name="I2" border="0" frameborder="0" height="143" title="-" scrolling="no" align="top" src="http://elementalpt.ddns.net/include/lidersod.php" width="219"></iframe>
+					</div>
+				</div>
+				<div class="banner-block flex-c-c"><br><br>&nbsp;</div>
+				<div class="banner-FB">&nbsp;</div>
+			</aside>
+		</div>
+		<footer class="footer">
+			<div id="toTop"></div>
+			<div class="footer-block-t flex-s-c">
+				<ul class="f-menu footer-block-l">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="../painel/cadtermos.php">Cadastro</a></li>
+					<li><a href="download.php">Downloads</a></li>
+					<li><a href="rankings.php">Rankings</a></li>
+					<li><a href="guia.php">Guia do Jogo</a></li>
+					<li><a href="#">Regras</a></li>
+					<li><a href="donate.php">Donate</a></li>
+				</ul>
+				<div class="soc-block footer-block-r">
+					<a href="https://www.facebook.com/ElementalPT" class="facebook" target="_blank"></a>
+					<a href="/ElementalPT" class="twitter"></a>
+					<a href="/ElementalPT" class="twitch"></a>
+					<a href="/ElementalPT" class="youtube"></a>
+				</div>
+			</div>
+			<div class="footer-block-b flex-s">
+				<div class="copyright footer-block-l">
+				</div>
+			</div>
+		</footer>
+	</div>
+	<script src="../ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/global.js"></script>
+</body>
+
+</html>
