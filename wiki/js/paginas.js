f8c57307ec83ab8b37a0cd11e109d89e456656a4
Deivison Soares <deivisonsoares1610@outlook.com>
2022-04-20 05:20:47 -0300

    Update 20-04-22
    Todas atts implementadas até o momento

---
+++ wiki/js/paginas.js
@@ -0,0 +1,75 @@
+function ola(){
+    alert('Olá Guerreiro, seja muito bem vindo ao Elemental Wiki!')
+}
+
+function aneis(){
+    alert('Você está na aba de Anéis!')
+}
+
+function arcos(){
+    alert('Você está na aba de Arcos!')
+}
+
+function armaduras(){
+    alert('Você está na aba de Armaduras!')
+}
+
+function botas(){
+    alert('Você está na aba de Botas!')
+}
+
+function braceletes(){
+    alert('Você está na aba de Braceletes!')
+}
+
+function brincos(){
+    alert('Você está na aba de Brincos!')
+}
+
+function cajadoevarinha(){
+    alert('Você está na aba de Cajados e Varinhas!')
+}
+
+function colares(){
+    alert('Você está na aba de Colares!')
+}
+
+function escudos(){
+    alert('Você está na aba de Escudos!')
+}
+
+function espadas(){
+    alert('Você está na aba de Espadas!')
+}
+
+function foices(){
+    alert('Você está na aba de Foices!')
+}
+
+function garras(){
+    alert('Você está na aba de Garras!')
+}
+
+function lancas(){
+    alert('Você está na aba de Lanças!')
+}
+
+function luvas(){
+    alert('Você está na aba de Luvas!')
+}
+
+function machados(){
+    alert('Você está na aba de Machados!')
+}
+
+function martelos(){
+    alert('Você está na aba de Martelos!')
+}
+
+function orbitais(){
+    alert('Você está na aba de Orbitais!')
+}
+
+function robes(){
+    alert('Você está na aba de Robes!')
+}
