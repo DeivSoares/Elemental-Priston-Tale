ceec83bfe9ea0a572718e8caff96fae4cac91203
Deivison Soares <deivisonsoares1610@outlook.com>
2022-04-30 21:52:02 -0300

    .

--- wiki/js/paginas.js
+++ wiki/js/paginas.js
@@ -2,78 +2,78 @@ function ola(){
     alert('Olá Guerreiro, seja muito bem vindo ao Elemental Wiki!')
 }
 
-function aneis(){
-    alert('Você está na aba de Anéis!')
-}
+// function aneis(){
+//     alert('Você está na aba de Anéis!')
+// }
 
-function arcos(){
-    alert('Você está na aba de Arcos!')
-}
+// function arcos(){
+//     alert('Você está na aba de Arcos!')
+// }
 
-function armaduras(){
-    alert('Você está na aba de Armaduras!')
-}
+// function armaduras(){
+//     alert('Você está na aba de Armaduras!')
+// }
 
-function botas(){
-    alert('Você está na aba de Botas!')
-}
+// function botas(){
+//     alert('Você está na aba de Botas!')
+// }
 
-function braceletes(){
-    alert('Você está na aba de Braceletes!')
-}
+// function braceletes(){
+//     alert('Você está na aba de Braceletes!')
+// }
 
-function brincos(){
-    alert('Você está na aba de Brincos!')
-}
+// function brincos(){
+//     alert('Você está na aba de Brincos!')
+// }
 
-function cajadoevarinha(){
-    alert('Você está na aba de Cajados e Varinhas!')
-}
+// function cajadoevarinha(){
+//     alert('Você está na aba de Cajados e Varinhas!')
+// }
 
-function colares(){
-    alert('Você está na aba de Colares!')
-}
+// function colares(){
+//     alert('Você está na aba de Colares!')
+// }
 
-function escudos(){
-    alert('Você está na aba de Escudos!')
-}
+// function escudos(){
+//     alert('Você está na aba de Escudos!')
+// }
 
-function espadas(){
-    alert('Você está na aba de Espadas!')
-}
+// function espadas(){
+//     alert('Você está na aba de Espadas!')
+// }
 
-function foices(){
-    alert('Você está na aba de Foices!')
-}
+// function foices(){
+//     alert('Você está na aba de Foices!')
+// }
 
-function garras(){
-    alert('Você está na aba de Garras!')
-}
+// function garras(){
+//     alert('Você está na aba de Garras!')
+// }
 
-function lancas(){
-    alert('Você está na aba de Lanças!')
-}
+// function lancas(){
+//     alert('Você está na aba de Lanças!')
+// }
 
-function luvas(){
-    alert('Você está na aba de Luvas!')
-}
+// function luvas(){
+//     alert('Você está na aba de Luvas!')
+// }
 
-function machados(){
-    alert('Você está na aba de Machados!')
-}
+// function machados(){
+//     alert('Você está na aba de Machados!')
+// }
 
-function martelos(){
-    alert('Você está na aba de Martelos!')
-}
+// function martelos(){
+//     alert('Você está na aba de Martelos!')
+// }
 
-function orbitais(){
-    alert('Você está na aba de Orbitais!')
-}
+// function orbitais(){
+//     alert('Você está na aba de Orbitais!')
+// }
 
-function robes(){
-    alert('Você está na aba de Robes!')
-}
+// function robes(){
+//     alert('Você está na aba de Robes!')
+// }
 
-function sheltons(){
-    alert('Você está na aba de Sheltons!')
-}
+// function sheltons(){
+//     alert('Você está na aba de Sheltons!')
+// }
