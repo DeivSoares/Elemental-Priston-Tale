f736b133ad3d19f8ad0caf9c6bbbf2189fe51a18
Deivison Soares <deivisonsoares1610@outlook.com>
2022-05-13 05:35:25 -0300

    Att
    -Alterei as logos
    -Removi fotos não utilizadas para limpeza dos arquivos
    -Adicionei root css para trocar as cores com mais facilidade
    -Juntei 3 arquivos de css em 1 só de maneira organizada.

--- paineladm/ligarservidor.inc.php
+++ paineladm/ligarservidor.inc.php
@@ -1,32 +1,45 @@
 <?
-if (PT!=1) exit;
-if($_SESSION['permissao'] < 3){
-echo "Você não tem permissão para acessar esta área";
-exit;
+if (PT != 1) exit;
+if ($_SESSION['permissao'] < 3) {
+  echo "Vocï¿½ nï¿½o tem permissï¿½o para acessar esta ï¿½rea";
+  exit;
 }
 ?>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 
 <table background="imgs/fundo_textura1.gif" width="716" border="0" align="center" cellpadding="0" cellspacing="0">
-              <tr>
-                <td width="716"><table width="99%" border="0" align="center" cellpadding="4" cellspacing="2">
-                  <tr>
-                    <td colspan="2" align="center" bgcolor="#000000"><b><font color="#FFFFFF">Ligar o Servidor</font></b></td></table>
-                  <div align="center"><br />
-                  <? $fp = @fsockopen($iphost, $portasv, $errno, $errstr, 1); if($fp >= 1){  echo '<center><img src=imgs/on.gif>';} else{ echo '<center><img src=imgs/off.gif>'; } ?>
-                  <br />
-                  <br />
-      Para ligar o servidor, basta clicar no bot&atilde;o abaixo 'Ligar o Servidor'<br />
-      Atenção veja se o servidor não está ligado antes de clicar em ligar para não lagar o host executando 2 exes, ou então verifique se o admin não esta fazendo manutenção no momento.
-      <br /></div>
-    </div>
-                  </div>
-                  <form method="post" action="index.php?sessadm=ligarsv">
-                    <div align="center">
-        <label>
-          <input type="submit" class="button6" onclick ="return confirm('Você Tem certeza que deseja ligar o servidor!?')" value="Ligar o Servidor" /></label></div></form><br />
-    <strong>Instru&ccedil;&otilde;es de como Utilizar o Sistema de Ligar o Servidor<br />
-    </strong>1 Passo - Clique em ligar o servidor se realmente deseja ligar o servidor.<br />
-2 Passo - quando voc&ecirc; clicar em ligar o servidor automaticamente vai aparecer um alerta confirme oque deseja..<br />
-Se voc&ecirc; deseja realmente ligar o servidor clique em 'Ok' se quer cancelar a a&ccedil;&atilde;o clique em Cancelar<br />
-3 Passo - Apos confirma, vai aparecer que o servidor foi ligado com sucesso. </table>
+  <tr>
+    <td width="716">
+      <table width="99%" border="0" align="center" cellpadding="4" cellspacing="2">
+        <tr>
+          <td colspan="2" align="center" bgcolor="#000000"><b>
+              <font color="#FFFFFF">Ligar o Servidor</font>
+            </b></td>
+      </table>
+      <div align="center"><br />
+        <? $fp = @fsockopen($iphost, $portasv, $errno, $errstr, 1);
+        if ($fp >= 1) {
+          echo '<center><img src=imgs/on.gif>';
+        } else {
+          echo '<center><img src=imgs/off.gif>';
+        } ?>
+        <br />
+        <br />
+        Para ligar o servidor, basta clicar no bot&atilde;o abaixo 'Ligar o Servidor'<br />
+        Atenï¿½ï¿½o veja se o servidor nï¿½o estï¿½ ligado antes de clicar em ligar para nï¿½o lagar o host executando 2 exes, ou entï¿½o verifique se o admin nï¿½o esta fazendo manutenï¿½ï¿½o no momento.
+        <br />
+      </div>
+      </div>
+      </div>
+      <form method="post" action="index.php?sessadm=ligarsv">
+        <div align="center">
+          <label>
+            <input type="submit" class="button6" onclick="return confirm('Vocï¿½ Tem certeza que deseja ligar o servidor!?')" value="Ligar o Servidor" /></label>
+        </div>
+      </form><br />
+      <strong>Instru&ccedil;&otilde;es de como Utilizar o Sistema de Ligar o Servidor<br />
+      </strong>1 Passo - Clique em ligar o servidor se realmente deseja ligar o servidor.<br />
+      2 Passo - quando voc&ecirc; clicar em ligar o servidor automaticamente vai aparecer um alerta confirme oque deseja..<br />
+      Se voc&ecirc; deseja realmente ligar o servidor clique em 'Ok' se quer cancelar a a&ccedil;&atilde;o clique em Cancelar<br />
+      3 Passo - Apos confirma, vai aparecer que o servidor foi ligado com sucesso.
+</table>
